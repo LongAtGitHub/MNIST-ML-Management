a480ef531fb70f03a7e337649bbdf00b9f137254
LongAtGitHub <98071520+LongAtGitHub@users.noreply.github.com>
2024-02-26 20:03:30 -0600

    Revert "good model"
    This reverts commit c224d6a9c854bba3a5ed88011d856e0635717e3b.

--- model/simple_model.py
+++ model/simple_model.py
@@ -2,14 +2,15 @@ import torch
 import torch.nn as nn
 
 class SimpleMNISTModel(nn.Module):
-    def __init__(self, dropout_rate=0):
+    def __init__(self):
         super(SimpleMNISTModel, self).__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28*28, 256)  ,
+            nn.Linear(28*28, 512),
             nn.ReLU(),
-            nn.Dropout(dropout_rate),
-            nn.Linear(256, 10) ,
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
         )
 
     def forward(self, x):
